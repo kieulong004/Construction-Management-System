2a894f6c71044398f05678a2d317d1fe6d1913f5
kieulong004 <longvankieu2004@gmail.com>
2025-03-03 17:19:04 +0700

    udpate

--- src/components/config/api.ts
+++ src/components/config/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const intance = axios.create({
-    baseURL:import.meta.env.VITE_BASE_URL
+    baseURL:import.meta.env.VITE_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+      },
 })
 
 export default intance;
